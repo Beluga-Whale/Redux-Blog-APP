d2bffd818276853c377646a0e3b8eb3a811c5aa3
Beluga-Whale <savaddee159@gmail.com>
2022-10-17 20:24:51 +0700

    sort order post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -6,7 +6,8 @@ const initialState = [
         id: '1',
         title: 'Learning Redux Toolkit',
         content: "I've heard good things",
-        date: sub(new Date(), { minutes: 10 }).toISOString()
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+
     },
 
     {
