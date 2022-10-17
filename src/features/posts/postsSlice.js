9c2b560311f8ca59d2c6e2617a252b0d27a8f645
Beluga-Whale <savaddee159@gmail.com>
2022-10-17 20:33:41 +0700

    Add reaction emoji

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -7,14 +7,27 @@ const initialState = [
         title: 'Learning Redux Toolkit',
         content: "I've heard good things",
         date: sub(new Date(), { minutes: 10 }).toISOString(),
-
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     },
 
     {
         id: '2',
         title: 'SLices....',
         content: "The more I say slice, the more I want pizza",
-        date: sub(new Date(), { minutes: 10 }).toISOString()
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
     },
 
 ]
@@ -35,9 +48,23 @@ const postsSlice = createSlice({
                         title,
                         content,
                         date: new Date().toISOString(),
-                        userId
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
                     }
                 }
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
             }
         }
     }
@@ -45,6 +72,6 @@ const postsSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
