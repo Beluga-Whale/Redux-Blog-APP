a49c2eeebf09db943fc57264cca2cd46d64253a7
Beluga-Whale <savaddee159@gmail.com>
2022-10-17 20:20:04 +0700

    Add show date and author

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import PostAuthor from './PostAuthor'
 import { selectAllPosts } from './postsSlice'
+import TimeAgo from './TimeAgo'
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts)
@@ -9,12 +11,17 @@ const PostsList = () => {
         <article key={post.id} >
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
         </article>
     ))
     return (
         <section>
             <h2>Posts</h2>
             {renderedPosts}
+
         </section>
     )
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,12 +1,23 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things" },
+    {
+        id: '1',
+        title: 'Learning Redux Toolkit',
+        content: "I've heard good things",
+        date: sub(new Date(), { minutes: 10 }).toISOString()
+    },
 
-    { id: '2', title: 'SLices....', content: "The more I say slice, the more I want pizza" },
+    {
+        id: '2',
+        title: 'SLices....',
+        content: "The more I say slice, the more I want pizza",
+        date: sub(new Date(), { minutes: 10 }).toISOString()
+    },
 
 ]
-// 20:13 minute
+
 
 const postsSlice = createSlice({
     name: "posts",
@@ -16,12 +27,14 @@ const postsSlice = createSlice({
             reducer(state, action) {
                 state.push(action.payload)
             },
-            prepare(title, content) {
+            prepare(title, content, userId) {
                 return {
                     payload: {
                         id: nanoid(),
                         title,
-                        content
+                        content,
+                        date: new Date().toISOString(),
+                        userId
                     }
                 }
             }
